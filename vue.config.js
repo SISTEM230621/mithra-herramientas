147c7f74a9d647680a0ab4f26c97a5b4e6d8991b
Rafael <rrrrebolledohdz@gmail.com>
2021-07-19 15:40:34 -0500

    construir windows

--- vue.config.js
+++ vue.config.js
@@ -7,19 +7,17 @@ module.exports = {
       mainProcessWatch: [
         'src/main/background.js'
       ],
-      chainWebpackMainProcess: (config) => {
-        // Chain webpack config for electron main process only
-      },
-      appId: "com.mithra.app",
-      productName: "herramientas",
-      target: "NSIS",
-      directory: {
-        "output": "build"
-      },
-      nsis: {
-        "allowToChangeInstallationDirectory": true,
-        "oneClick": false
-      }
+      outputDir: "mithra-build",
+
+      // appId: "com.mithra.app",
+      // productName: "herramientas",
+      // directory: {
+      //   "output": "build"
+      // },
+      // nsis: {
+      //   "allowToChangeInstallationDirectory": true,
+      //   "oneClick": false
+      // }
     }
   },
   pages: {
@@ -27,11 +25,7 @@ module.exports = {
       entry: "src/renderer/main.js",
       template: "public/index.html",
       filename: "index.html",
-      // When using the page title option,
-      // The title tag in the template needs to be <title><%= htmlWebpackPlugin.options.title %></title>
       title: "Mithra",
-      // The blocks included in this page will be included by default
-      // The extracted generic chunk and vendor chunk.
       chunks: ["chunk-vendors", "chunk-common", "index"],
     },
   },
